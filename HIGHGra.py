b4626687bc64acf1b732888d6122cc7b8799c560
201524495 <49744580+201524495@users.noreply.github.com>
2019-06-11 15:03:10 +0900

    Update HIGHGra.py

--- HIGHGra.py
+++ HIGHGra.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import csv
 from matplotlib import font_manager, rc
 font_name = font_manager.FontProperties(fname='c:/Windows/Fonts/malgun.ttf').get_name()
 rc('font',family=font_name)
-import xml.etree.ElementTree as etree
-import HIGHWAY3api
-import sys
+import numpy as np
 
+
+def GRAPHY():
 #pip install pandas, pip install matplotlib
-df = pd.read_csv('CSVCSV.csv',engine='python')
+    df = pd.read_csv('CSVCSV.csv',engine='python')
 
+    plt.subplot(211)
+    plt.scatter(df['차종'],df['입출구'],df['교통량'])
+    plt.ylabel("교통량")
 
-plt.subplot(211)
-plt.bar(df['차종'],df['교통량'])
-plt.xlabel('')
-plt.ylabel("교통량")
+    plt.subplot(212)
+    plt.scatter(df['입출구'],df['교통량'])
+    plt.ylabel("교통량")
 
-plt.subplot(212)
-plt.bar(df['입출구'],df['교통량'])
-plt.xlabel('')
-plt.ylabel("교통량")
-
-plt.title('')
-plt.show()
-print(df)
+    plt.show()
+    print(df)
+if __name__ == "__main__":
+    GRAPHY()
